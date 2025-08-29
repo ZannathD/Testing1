3dd572a5459c81973f85a4d5fad56151d8bb2592
Night <du5tinl0ckhart555@gmail.com>
2025-08-28 22:35:00 -0400

    Boxes Showing up

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <SFML/Graphics.hpp>
 
+
+//Declaring functions
+void drawJeopardySquares(sf::RectangleShape shape, sf::RenderWindow& window);
+
+
 int main()
 {
+
+    // Jeopardy Boxes as rect
+    sf::RectangleShape rect;
+    rect.setFillColor(sf::Color::Blue);
+    rect.setSize(sf::Vector2f(1920/6, 1080/6));
+    rect.setOutlineColor(sf::Color::Black);
+    rect.setOutlineThickness(3);
+
+
+
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
@@ -14,8 +29,33 @@ int main()
                 window.close();
             }
         }
+/*
+ *
+ *  get mouse position over boxes for clicks
+ *
+ *
+ *
+ *
+ */
+
 
         window.clear();
+
+        drawJeopardySquares(rect, window);
+
         window.display();
     }
 }
+
+
+void drawJeopardySquares(sf::RectangleShape shape, sf::RenderWindow& window)
+{
+    for (int x = 0; x < 6; x++)
+    {
+        for (int y = 0; y < 6; y++)
+        {
+            shape.setPosition(sf::Vector2f(x * (1920 / 6), y * (1080u / 6)));
+            window.draw(shape);
+        }
+    }
+};
